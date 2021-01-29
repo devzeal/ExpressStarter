072b4e173c4fbcdc48bcfdfa4c3cb6645791f3fe
devlooskie <hello@looskie.com>
2021-01-28 22:03:57 -0500

    use cleaner stuff, we'll need ejs and acouple of other thigns with this

--- starter.js
+++ starter.js
@@ -4,7 +4,7 @@ const app = express();
 var PORT = 3000; // Feel free to link your dotenv here
 
 app.get('/', (req, res) => {
-  res.sendFile('./index.html', { root: __dirname });
+  res.render('index.html');
 });
 
 app.listen(PORT, () => console.log(`Website at http://localhost:3000`)) // You can replace 3000 with ${PORT}
