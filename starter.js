60b73c756647b7c92515d9350d5dfc4170d8f30b
devlooskie <hello@looskie.com>
2021-01-28 22:05:17 -0500

    use ejs as html, this is so then we can have res.render() instead of res.sendFile(), sendFile()  is ugly lololoololol

--- starter.js
+++ starter.js
@@ -6,6 +6,7 @@ var PORT = 3000; // Feel free to link your dotenv here
 // Some stupid shit express needs
 app.use(express.static('./public')); // This is so then express reads your css/js/img files :)
 app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
   res.render('index.html');
