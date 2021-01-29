ec3bf9f6e2b9d0fc047e9970ba5cb20112ead27b
Zeskey <74628555+Zeskey@users.noreply.github.com>
2021-01-28 21:11:07 -0500

    Create starter.js

---
+++ starter.js
@@ -0,0 +1,11 @@
+//Feel free to change the file name 
+const express = require('express');
+var PORT = 3000; //Feel free to link your dotenv here
+
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', { root: __dirname });
+});
+
+app.listen(PORT, () => {
+  console.log(`Website at http://localhost:3000` // You can replace 3000 with ${PORT});
+});
